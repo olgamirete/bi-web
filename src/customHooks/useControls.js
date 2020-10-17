c1ac6052ae99e551ec126574f98aa109e439c2e8
olgamirete <olgamirete@gmail.com>
2020-10-17 12:56:57 -0300

    removed event handlers

--- src/customHooks/useControls.js
+++ src/customHooks/useControls.js
@@ -1,57 +1,56 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 
-function useControls(keyboardInfo) {
+function useControls() {
 
   const [flagShowSidePanel, setFlagShowSidePanel] = useState(true);
   const [flagSnapToGrid, setFlagSnapToGrid] = useState(false);
   const [flagDisplayGrid, setFlagDisplayGrid] = useState(true);
-  const [lockGridSetting, setLockGridSetting] = useState(false);
+  // const [lockGridSetting, setLockGridSetting] = useState(false);
 
   const toggleSidePanel = () => {
     setFlagShowSidePanel(!flagShowSidePanel);
   }
-  
+
   const toggleSnapToGrid = useCallback((e) => {
     setFlagSnapToGrid(!flagSnapToGrid);
   }, [flagSnapToGrid]);
-  
+
   const toggleDisplayGrid = useCallback((e) => {
     setFlagDisplayGrid(!flagDisplayGrid);
   }, [flagDisplayGrid]);
 
 
-  // Handle Alt key press and release.------------------------------------------
-  useEffect(() => {
-    if (keyboardInfo.altPressed === true) {
-      if (lockGridSetting === false) {
-        setLockGridSetting(true);
-        toggleSnapToGrid();
-      }
-    } else {
-      if (lockGridSetting === true) {
-        setLockGridSetting(false);
-        toggleSnapToGrid();
-      }
-    }
-  }, [keyboardInfo.altPressed, lockGridSetting, toggleSnapToGrid]);
-  // ---------------------------------------------------------------------------
+  // // Handle Alt key press and release.------------------------------------------
+  // useEffect(() => {
+  //   if (keyboardInfo.altPressed === true) {
+  //     if (lockGridSetting === false) {
+  //       setLockGridSetting(true);
+  //       toggleSnapToGrid();
+  //     }
+  //   } else {
+  //     if (lockGridSetting === true) {
+  //       setLockGridSetting(false);
+  //       toggleSnapToGrid();
+  //     }
+  //   }
+  // }, [keyboardInfo.altPressed, lockGridSetting, toggleSnapToGrid]);
+  // // ---------------------------------------------------------------------------
 
-  
 
-const controls = {
-  flags: {
+
+  const controlFlags = {
     showSidePanel: flagShowSidePanel,
     snapToGrid: flagSnapToGrid,
     displayGrid: flagDisplayGrid
-  },
-  methods: {
+  };
+  const controlMethods = {
+    setFlagShowSidePanel: setFlagShowSidePanel,
     toggleSidePanel: toggleSidePanel,
     toggleSnapToGrid: toggleSnapToGrid,
     toggleDisplayGrid: toggleDisplayGrid
-  }
-}
+  };
 
-return controls;
+  return [controlFlags, controlMethods];
 }
 
 export default useControls;
