3e21927eb5525683f6f78552f6b79e914ae153b6
olgamirete <olgamirete@gmail.com>
2020-10-17 12:55:22 -0300

    added handler for pointer down events

--- src/components/side-panel/SidePanel.js
+++ src/components/side-panel/SidePanel.js
@@ -5,34 +5,40 @@ import FieldsList from './components/fields-list/FieldsList.js';
 function SidePanel(props) {
 
   const [activeSection, setActiveSection] = useState("fields");
-  const activateSection = (e) => {
-    let section = e.target.dataset.section;
-    if (section === undefined) {
-      section = e.target.parentNode.dataset.section;
+  
+  const handlePointerDown = (e) => {
+    let selectedSection = e.target.dataset.section;
+    if (selectedSection === undefined) {
+      selectedSection = e.target.parentNode.dataset.section;
     }
-    setActiveSection(section);
+    if(activeSection !== selectedSection) {
+      setActiveSection(selectedSection);
+      props.controlMethods.setFlagShowSidePanel(true);
+    } else {
+      props.controlMethods.toggleSidePanel();
+    }
   }
 
   return (
-    <div className={"side-panel" + (props.flagShow ? "" : " hide")}>
+    <div className={"side-panel" + (props.controlFlags.showSidePanel ? "" : " hide")}>
 
       <div className="buttons-container">
         <button
           data-section="fields"
           className={activeSection === "fields" ? "active" : ""}
-          onPointerDown={activateSection}>
+          onPointerDown={handlePointerDown}>
           <span className="text">Fields</span>
         </button>
         <button
           data-section="format"
           className={activeSection === "format" ? "active" : ""}
-          onPointerDown={activateSection}>
+          onPointerDown={handlePointerDown}>
           <span className="text">Format</span>
         </button>
         <button
           data-section="other"
           className={activeSection === "other" ? "active" : ""}
-          onPointerDown={activateSection}>
+          onPointerDown={handlePointerDown}>
           <span className="text">Other</span>
         </button>
       </div>
