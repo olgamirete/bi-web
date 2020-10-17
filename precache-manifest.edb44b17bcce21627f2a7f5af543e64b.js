447ad7654bae1f514a8d7508c7af5c75d5ac1c35
olgamirete <olgamirete@gmail.com>
2020-10-17 13:09:44 -0300

    Updates

---
+++ precache-manifest.edb44b17bcce21627f2a7f5af543e64b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15367a8a6388d14854ca98a591921e23",
+    "url": "/bi-web/index.html"
+  },
+  {
+    "revision": "2d0e9dfee920f267471d",
+    "url": "/bi-web/static/css/main.6196eceb.chunk.css"
+  },
+  {
+    "revision": "edaa75e7d5d68ef51d05",
+    "url": "/bi-web/static/js/2.ee41f74d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bi-web/static/js/2.ee41f74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0e9dfee920f267471d",
+    "url": "/bi-web/static/js/main.054688bd.chunk.js"
+  },
+  {
+    "revision": "ba6e68208be84447530f",
+    "url": "/bi-web/static/js/runtime-main.d9002fe4.js"
+  },
+  {
+    "revision": "d3085f686df272f9e1a267cc69b2d24f",
+    "url": "/bi-web/static/media/Montserrat-Bold.d3085f68.ttf"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/bi-web/static/media/Montserrat-Regular.07689d4e.ttf"
+  }
+]);
