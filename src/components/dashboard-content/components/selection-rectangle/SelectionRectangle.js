af05d808039a6c2969dab4d8eea734dd94250157
olgamirete <olgamirete@gmail.com>
2020-10-17 12:53:57 -0300

    created a component for the selection rectangle

---
+++ src/components/dashboard-content/components/selection-rectangle/SelectionRectangle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SelectionRectangle.css';
+
+function SelectionRectangle(props) {
+    // console.log(props.flagDraw);
+    return (
+        <div
+            className={"selection-rectangle" + (props.selRectInfo.flagDraw ? "" : " hide")}
+            style={{
+                left: props.selRectInfo.pos.left,
+                top: props.selRectInfo.pos.top,
+                width: props.selRectInfo.size.width,
+                height: props.selRectInfo.size.height}}>
+        </div>
+    );
+}
+
+export default SelectionRectangle;
