db0d5953be0ccaa77ba5d1133f2257e2ed2d3efc
olgamirete <olgamirete@gmail.com>
2020-10-17 12:52:17 -0300

    extracted the grid as a separate component

--- src/components/dashboard-content/DashboardContent.js
+++ src/components/dashboard-content/DashboardContent.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './DashboardContent.css';
-import MetaCard from '../meta-card/MetaCard.js';
-import useCardEventHandlers from './customHooks/useCardEventHandlers.js';
+import MetaCard from './components/meta-card/MetaCard.js';
+import useDashboardEventHandlers from './customHooks/useDashboardEventHandlers.js';
+import SelectionRectangle from './components/selection-rectangle/SelectionRectangle.js';
+import Grid from './components/grid/Grid.js';
 
 const DASHBOARD_ID = "dashboard-content";
 
 function DashboardContent(props) {
 
-  const [cardFlags, debugInfo] = useCardEventHandlers(
+  const dashboardRef = useRef(null);
+
+  const [dashboardFlags, dashboardProps] = useDashboardEventHandlers(
     DASHBOARD_ID,
     props.cardMethods,
-    props.controlFlags,
-    props.keyboardInfo);
+    props.controlMethods);
 
   return (
     <div
       id={DASHBOARD_ID}
-      className="dashboard-content">
-      {
-        props.controlFlags.displayGrid === true &&
-        <span>
-          <div className="smallgrid"></div>
-          <div className="grid"></div>
-        </span>
-      }
-      <span className="debug-info">{debugInfo}</span>
+      className="dashboard-content"
+      ref={dashboardRef}>
+      <Grid
+        controlFlags={props.controlFlags}
+        dashboardRef={dashboardRef}
+        dashboardScrollSize={dashboardProps.scrollSize} />
+      {/* <span className="debug-info">{dashboardProps.debugInfo}</span> */}
+      {/* <span className="debug-info">{JSON.stringify(dashboardProps.scrollSize)}</span> */}
       {
         Array.from(props.cards.values()).map((cardInfo, i, arr) => {
           return <MetaCard
             key={i}
             cardInfo={cardInfo}
-            dashboardFlags={cardFlags}
+            dashboardFlags={dashboardFlags}
             controlFlags={props.controlFlags} />
         })
       }
+      <SelectionRectangle selRectInfo={dashboardProps.selectionRectangleProps} />
     </div>
   );
 }
