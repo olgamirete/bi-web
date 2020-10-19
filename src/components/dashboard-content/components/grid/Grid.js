bb449578b2ca5755355aa0300c7d5613ac192f09
olgamirete <olgamirete@gmail.com>
2020-10-19 02:01:14 -0300

    removed old stuff

--- src/components/dashboard-content/components/grid/Grid.js
+++ src/components/dashboard-content/components/grid/Grid.js
@@ -2,49 +2,9 @@ import React from 'react';
 import './Grid.css';
 
 function Grid(props) {
-
-  // const [dashboardScrollSize, setDashboardScrollSize] = useState({
-  //   width: "100%",
-  //   height: "100%"
-  // });
-
-  // const getScrollSize = useCallback(() => {
-  //   if (props.dashboardRef.current === null) {
-  //     return dashboardScrollSize;
-  //   } else {
-  //     return {
-  //       width: props.dashboardRef.current.scrollWidth,
-  //       height: props.dashboardRef.current.scrollHeight
-  //     }
-  //   }
-  // }, [props.dashboardRef, dashboardScrollSize]);
-
-
-  // useEffect(() => {
-  //   // const dashboard = props.dashboardRef.current;
-  //   // const scrollWidth = dashboard === null ? 0 : dashboard.scrollWidth;
-  //   // const scrollHeight = dashboard === null ? 0 : dashboard.scrollHeight;
-  //   // alert(scrollWidth + "-" + scrollHeight);
-  //   // alert(JSON.stringify(getScrollSize(dashboard)));
-  //   // console.log(JSON.stringify(getScrollSize(dashboard)));
-  //   const newScrollSize = getScrollSize();
-  //   if(dashboardScrollSize !== newScrollSize){
-  //     setDashboardScrollSize(newScrollSize);
-  //     console.log(false);
-  //   } else {
-  //     console.log(true);
-  //   }
-  //   // alert("ran");
-
-  // }, [props.dashboardRef, getScrollSize, dashboardScrollSize]);
-
   return (
     <div
-      className="grid"
-      style={{
-        width: props.dashboardScrollSize.width,
-        height: props.dashboardScrollSize.height
-      }}>
+      className="grid" >
       {
         props.controlFlags.displayGrid === true &&
         <span>
