32cf8eefb22cdd28a4b9aee16ecc0c914bed6551
olgamirete <olgamirete@gmail.com>
2020-11-08 09:58:04 -0300

    added buttons for changing the card type

--- src/components/side-panel/sections/card-format/CardFormat.js
+++ src/components/side-panel/sections/card-format/CardFormat.js
@@ -5,7 +5,24 @@ function CardFormat(props) {
   return (
     <section className={"section" + (props.activeSection === "format" ? " active" : "")}>
       <h3 className="title">Format</h3>
-          Format options
+          <h4>Set chart type</h4>
+          <button
+            onPointerDown={() => props.cardMethods.setContentTypeOfSelectedCards("barchart")} >
+            Bar chart
+          </button>
+          <button
+            onPointerDown={() => props.cardMethods.setContentTypeOfSelectedCards("piechart")} >
+            Pie chart
+          </button>
+          <button
+            onPointerDown={() => props.cardMethods.setContentTypeOfSelectedCards("linechart")} >
+            Line chart
+          </button>
+          <h4>Set chart options</h4>
+          <button
+            onPointerDown={props.cardMethods.toggleSelectedCardsLegend} >
+            Toggle legend
+          </button>
     </section>
   );
 }
