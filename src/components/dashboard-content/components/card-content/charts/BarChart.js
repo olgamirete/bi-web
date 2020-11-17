5db86f0c65963b89d7af0ec6cc67ab90b2287b26
olgamirete <olgamirete@gmail.com>
2020-11-17 00:39:12 -0300

    initial draft for scatter plot

--- src/components/dashboard-content/components/card-content/charts/BarChart.js
+++ src/components/dashboard-content/components/card-content/charts/BarChart.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import './BarChart.css';
 
-// const data = [
-//   {x: 0, y: 0},
-//   {x: 10, y: 10},
-//   {x: 20, y: 15},
-//   {x: 30, y: 20},
-//   {x: 40, y: 25},
-//   {x: 50, y: 30},
-//   {x: 60, y: 40},
-//   {x: 70, y: 5},
-//   {x: 80, y: 20},
-//   {x: 90, y: 10},
-//   {x: 100, y: 0},
-//   {x: 110, y: 0}
-// ];
-
 function BarChart(props) {
 
   const chartWidth = props.cardInfo.size.width - 2 * props.borderWidth;
@@ -32,12 +17,49 @@ function BarChart(props) {
   const axisHorizontalLength = axisMarginRight - axisMarginLeft;
   const axisVerticalLength = axisMarginBottom - axisMarginTop;
 
+  const axisHorizontalUsableLength = axisHorizontalLength - axisMarginLeft;
+  const axisVerticalUsableLength = axisVerticalLength - (chartHeight - axisMarginBottom);
+
+  const pointRadius = Math.min(3, 0.01*Math.sqrt(Math.pow(axisHorizontalUsableLength, 2) + Math.pow(axisVerticalUsableLength, 2)));
+
   const tickMinorLength = 5;
   const tickMajorLength = 8;
 
-  // const xMin = 10;
-  // const xMax = 10;
-  
+  const xMin = 0;
+  const xMax = 100;
+  const yMin = 0;
+  const yMax = 50;
+
+  const xMinorStep = 5;
+  const xMajorStep = 10;
+  const yMinorStep = 5;
+  const yMajorStep = 10;
+
+  const xTickCount = Math.ceil((xMax - xMin) / xMinorStep);
+  const yTickCount = Math.ceil((yMax - yMin) / yMinorStep);
+
+  let xTicksAux = [];
+  for (let i = 0; i < xTickCount; i++) {
+    let tickLength = ((i + 1) * xMinorStep % xMajorStep === 0) ? tickMajorLength : tickMinorLength;
+    xTicksAux.push(<path d={"M " + (originLeft + ((i + 1) / xTickCount) * axisHorizontalUsableLength) + "," + originTop + " v " + tickLength} />);
+  }
+  const xTicks = <g className="axis-ticks" >{xTicksAux}</g>;
+
+  let yTicksAux = [];
+  for (let i = 0; i < yTickCount; i++) {
+    let tickLength = ((i + 1) * yMinorStep % yMajorStep === 0) ? tickMajorLength : tickMinorLength;
+    yTicksAux.push(<path d={"M " + originLeft + ", " + (originTop - ((i + 1) / yTickCount) * axisVerticalUsableLength) + " h -" + tickLength} />);
+  }
+  const yTicks = <g className="axis-ticks">{yTicksAux}</g>;
+
+  let pointsAux = [];
+  props.data.forEach(d => {
+    const xCoord = originLeft + d.x * (axisHorizontalUsableLength / (xMax - xMin));
+    const yCoord = originTop - d.y * (axisVerticalUsableLength / (yMax - yMin));
+    pointsAux.push(<circle className="point" cx={xCoord} cy={yCoord} r={pointRadius} />);
+  });
+  const points = <g>{pointsAux}</g>;
+
   return (
     <svg xmlns="http://www.w3.org/2000/svg"
       viewBox={"0 0 " + chartWidth + " " + chartHeight}
@@ -55,66 +77,31 @@ function BarChart(props) {
       </g>
 
       {/* <!-- Ejes --> */}
-      <g>
+      <g className="axis" >
         <g>
           {/* <!-- Eje horizontal --> */}
-          <g>
-            <path d={"M " + axisMarginLeft + "," + originTop + " h " + axisHorizontalLength} />
-            <g>
-              {/* <!-- Axis ticks --> */}
-              <path d={"M 30," + originTop + " v " + tickMinorLength} />
-              <path d={"M 40," + originTop + " v " + tickMinorLength} />
-              <path d={"M 50," + originTop + " v " + tickMinorLength} />
-              <path d={"M 60," + originTop + " v " + tickMinorLength} />
-              <path d={"M 70," + originTop + " v " + tickMajorLength} />
-              <path d={"M 80," + originTop + " v " + tickMinorLength} />
-              <path d={"M 90," + originTop + " v " + tickMinorLength} />
-              <path d={"M 100," + originTop + " v " + tickMinorLength} />
-              <path d={"M 110," + originTop + " v " + tickMinorLength} />
-              <path d={"M 120," + originTop + " v " + tickMajorLength} />
-            </g>
-          </g>
-
+          <path d={"M " + axisMarginLeft + "," + originTop + " h " + axisHorizontalLength} />
+          {/* <!-- Axis ticks --> */}
+          {xTicks}
+        </g>
+        <g>
           {/* <!-- Eje vertical --> */}
-          <g>
-            <path d={"M " + originLeft + "," + axisMarginTop + " v " + axisVerticalLength} />
-            <g>
-              {/* <!-- Axis ticks --> */}
-              <path d={"M " + originLeft + ", 110 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 100 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 90 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 80 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 70 h -" + tickMajorLength} />
-              <path d={"M " + originLeft + ", 60 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 60 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 50 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 40 h -" + tickMinorLength} />
-              <path d={"M " + originLeft + ", 30 h -" + tickMajorLength} />
-            </g>
-          </g>
+          <path d={"M " + originLeft + "," + axisMarginTop + " v " + axisVerticalLength} />
+          {/* <!-- Axis ticks --> */}
+          {yTicks}
         </g>
       </g>
 
       {/* <!-- Grilla --> */}
-      <g>
-        <g>
-          <g>
-            {/* <!-- <path d="m 63.210516,71.81547 v 80.88692" /> --> */}
-            {/* <!-- <path d="M 80.726683,71.815471 V 152.70239" /> --> */}
-            {/* <!-- <path d="M 99.765995,71.815464 V 152.70239" /> --> */}
-          </g>
-          <g>
-            {/* <!-- <path d="m 183.69642,90.246341 -137.583327,10e-7" /> --> */}
-            {/* <!-- <path d="M 183.69642,115.13125 H 46.113087" /> --> */}
-          </g>
-        </g>
-      </g>
 
       {/* <!-- Barras --> */}
-      <g>
+      {/* <g>
         <rect x="20" y="60" width="20" height="20" className="bar" />
         <rect x="60" y="40" width="20" height="50" className="bar" />
-      </g>
+      </g> */}
+
+      {/* <!-- Puntos --> */}
+      {points}
 
     </svg>
   );
