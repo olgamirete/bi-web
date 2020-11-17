fb8c66f012f601c992b1b5b7b2066f67116c7ac4
olgamirete <olgamirete@gmail.com>
2020-11-16 22:26:32 -0300

    added persistance to selected theme

--- src/customHooks/useConfig.js
+++ src/customHooks/useConfig.js
@@ -1,12 +1,39 @@
 import { useState } from 'react';
+const VALID_THEMES = [
+  "theme-1",
+  "theme-2",
+  "theme-3",
+  "theme-4",
+  "theme-5",
+  "theme-6",
+]
 
 function useConfig() {
 
-  const [theme, setTheme] = useState("theme-1");
+  const defaultTheme = VALID_THEMES[0];
+  const storedTheme = localStorage.getItem("user-theme");
+  const initialTheme = storedTheme === null ? defaultTheme : storedTheme;
+
+  const [theme, setTheme] = useState(initialTheme);
+
+  const changeTheme = (newTheme) => {
+    if(VALID_THEMES.includes(newTheme)) {
+      setTheme(newTheme);
+      localStorage.setItem("user-theme", newTheme);
+    } else {
+      throw new Error("The theme '" + newTheme + "' does not exist.");
+    }
+  }
+
+  const clearTheme = () => {
+    localStorage.removeItem("user-theme");
+    setTheme(defaultTheme);
+  }
 
   const config = {
     theme: theme,
-    setTheme: setTheme
+    setTheme: changeTheme,
+    clearTheme: clearTheme
   }
 
   return config
