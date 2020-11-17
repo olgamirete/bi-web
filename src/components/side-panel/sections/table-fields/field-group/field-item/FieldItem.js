1b62559cdbf399f7c12e1f8e9b3b5e29d10c491c
olgamirete <olgamirete@gmail.com>
2020-11-16 22:11:29 -0300

    improved styling of items on drag

--- src/components/side-panel/sections/table-fields/field-group/field-item/FieldItem.js
+++ src/components/side-panel/sections/table-fields/field-group/field-item/FieldItem.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FieldItem.css';
 
 function FieldItem(props) {
+  const [dragging, setDragging] = useState(false);
+
+  function handlerPointerDown(e) {
+    setDragging(true);
+  }
   function handlerDragStart(e) {
     e.dataTransfer.setData("text", e.target.id);
   }
+
+  function handlerPointerUp(e) {
+    setDragging(false);
+  }
+  function handlerDragEnd(e) {
+    setDragging(false);
+  }
+
   return (
     <li
       // key={props.key}
-      className="field-item"
-      draggable="true" >
-      <span
-        id={"table-" + props.tableId + "-field-" + props.fieldName}
-        draggable="true"
-        onDragStart={handlerDragStart} >
-          {props.fieldName}
-      </span>
+      className={"field-item" + (dragging ? " dragging" : "")}
+      draggable="true"
+      id={"table-" + props.tableId + "-field-" + props.fieldName}
+      onPointerDown={handlerPointerDown}
+      onPointerUp={handlerPointerUp}
+      onDragStart={handlerDragStart}
+      onDragEnd={handlerDragEnd} >
+      {props.fieldName}
     </li>
   );
 }
