c1ec33549f135958b29dd648b413e538af888ab0
olgamirete <olgamirete@gmail.com>
2020-11-20 00:02:42 -0300

    implemented test pie chart with victory library

--- src/components/dashboard-content/components/card-content/charts/PieChart.js
+++ src/components/dashboard-content/components/card-content/charts/PieChart.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './PieChart.css';
+import { VictoryPie } from "victory";
 
 function PieChart(props) {
   return (
-    <svg
-      className="pie-chart">
-      <text x="0" y="20">Pie chart</text>
-      <rect cx="0" cy="0" width="10" height="10" />
-    </svg>
+    // <svg
+    //   className="pie-chart">
+    //   <text x="0" y="20">Pie chart</text>
+    //   <rect cx="0" cy="0" width="10" height="10" />
+    // </svg>
+    <VictoryPie
+      colorScale={["black", "var(--cg-blue)", "green"]}
+      data={props.data} />
   );
 }
 
